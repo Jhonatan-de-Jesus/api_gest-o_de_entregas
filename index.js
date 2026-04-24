bfcc773d4d7ca3c47e3c5d120338cabcf7433654
Jhonatan de jesus caldeira <jhonatancaldeiraa@gmail.com>
2026-04-24 15:25:13 -0300

    Primeira Atualização

---
+++ index.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://qkxdobhzmwfabalzzcpu.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_zVSvmGHwQar-3sZnskHWpQ_hsb7cjDY';
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
