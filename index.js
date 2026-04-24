c5dcadef5d4de15c7c43d0ac6abe64a3a025feaa
Jhonatan de jesus caldeira <jhonatancaldeiraa@gmail.com>
2026-04-24 15:32:08 -0300

    Adiciona função para criar entrega

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ const SUPABASE_URL = 'https://qkxdobhzmwfabalzzcpu.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_zVSvmGHwQar-3sZnskHWpQ_hsb7cjDY';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function criarEntrega({ motorista_id, veiculo_id, destino, status = 'pendente' }) {
+  const { data, error } = await supabase
+    .from('entregas')
+    .insert({
+      motorista_id,
+      veiculo_id,
+      destino,
+      status
+    })
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  return data;
+}
